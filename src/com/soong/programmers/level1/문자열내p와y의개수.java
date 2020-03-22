b15f810b196268b0de26c0cb131528625dd947ff
seungyeon <zm_mz@naver.com>
2020-03-22 23:50:42 +0900

    Etc programmers-level1 [문자열내p와y의개수]

--- src/com/soong/programmers/level1/문자열내p와y의개수.java
+++ src/com/soong/programmers/level1/문자열내p와y의개수.java
@@ -1,8 +1,31 @@
 package com.soong.programmers.level1;
 
-import sun.text.normalizer.UTF16;
-
 public class 문자열내p와y의개수 {
+
+    /*
+    문제 설명)
+    대문자와 소문자가 섞여있는 문자열 s가 주어집니다.
+    s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요.
+    'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
+
+    예를 들어 s가 pPoooyY면 true를 return하고 Pyy라면 false를 return합니다.
+
+    제한사항)
+    문자열 s의 길이 : 50 이하의 자연수
+    문자열 s는 알파벳으로만 이루어져 있습니다.
+
+    입출력 예)
+    s	answer
+    pPoooyY	true
+    Pyy	false
+
+    입출력 예 설명)
+    입출력 예 #1
+    'p'의 개수 2개, 'y'의 개수 2개로 같으므로 true를 return 합니다.
+
+    입출력 예 #2
+    'p'의 개수 1개, 'y'의 개수 2개로 다르므로 false를 return 합니다.
+    */
 
     public static void main(String[] args) {
         System.out.println(solution("pPppoooyY Yyooo"));
@@ -17,8 +40,8 @@ public class 문자열내p와y의개수 {
         int y = 0;
 
         for (String letter : text) {
-            if (letter.equals("p")) p ++;
-            if (letter.equals("y")) y ++;
+            if (letter.equals("p")) p++;
+            if (letter.equals("y")) y++;
         }
         return  p == y ? true : false;
     }
